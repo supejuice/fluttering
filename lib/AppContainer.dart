3b43e2fd5947acc54a070a05f0aa85b6544a1bc4
C218718 <Apples@72>
2023-12-06 14:20:21 +0530

    updated modifiers

--- lib/AppContainer.dart
+++ lib/AppContainer.dart
@@ -3,9 +3,9 @@ import 'package:fluttering/catfact/data/CatFactRepository.dart';
 import 'package:fluttering/catfact/data/CatFactService.dart';
 
 class AppContainer {
-  late Dio dioClient = Dio();
-  // ..options.baseUrl = "https://meowfacts.herokuapp.com/";
-  late var catFactService = CatFactService(dioClient, baseUrl: "https://meowfacts.herokuapp.com/");
-  late CatFactRepository catFactRepository = CatFactRepositoryImpl(catFactService: catFactService);
-
+  late final Dio _dioClient = Dio();
+  late final _catFactService =
+      CatFactService(_dioClient, baseUrl: "https://meowfacts.herokuapp.com/");
+  late final CatFactRepository catFactRepository =
+      CatFactRepositoryImpl(catFactService: _catFactService);
 }
